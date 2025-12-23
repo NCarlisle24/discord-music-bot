ea13aecfb1d0364c5f42b7b7a2e61ab2f0938e04
NCarlisle24 <lorenzonolan12983@gmail.com>
2025-12-22 18:42:29 -1000

    Added bot initialization functions

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world!")
+# ------------------------- imports ------------------------- #
+
+import discord
+from discord.ext import commands
+import os
+
+# ------------------------- preamble ------------------------- #
+
+# get info from the env file
+BOT_KEY = os.getenv("BOT_KEY")
+
+# specify what the bot will access
+intents = discord.Intents.default()
+intents.message_content = True
+
+# create the bot
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# setup event
+@bot.event
+async def on_ready():
+    print(f'{bot.user} connected to Discord!')
+
+# ------------------------- begin commands ------------------------- #
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send("test message")
+
+bot.run(BOT_KEY)
