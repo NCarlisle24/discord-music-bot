51e5f1cb712cab032962e911ff6f47e7066a89ac
NCarlisle24 <lorenzonolan12983@gmail.com>
2025-12-22 20:32:24 -1000

    Added bot joining and leaving voice channels

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-# ------------------------- imports ------------------------- #
+# ---------------------------------------------- imports ---------------------------------------------- #
 
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 import os
 
-# ------------------------- preamble ------------------------- #
+# ---------------------------------------------- preamble ---------------------------------------------- #
+
+# constants
+COMMAND_PREFIX = "!"
+TRACKED_CHANNEL_IDS = [1400122378222174382]
 
 # get info from the env file
+load_dotenv()
 BOT_KEY = os.getenv("BOT_KEY")
 
 # specify what the bot will access
@@ -14,17 +20,79 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 # create the bot
-bot = commands.Bot(command_prefix='!', intents=intents)
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
-# setup event
+# ---------------------------------------------- begin events ---------------------------------------------- #
+
 @bot.event
 async def on_ready():
     print(f'{bot.user} connected to Discord!')
 
-# ------------------------- begin commands ------------------------- #
+@bot.event
+async def on_message(message):
+    if message.author == bot.user or message.channel.id not in TRACKED_CHANNEL_IDS:
+        return
+    
+    print(f"Received message from user {message.author}: {message.content}")
+
+    await bot.process_commands(message)
+
+# ---------------------------------------------- begin helper functions ---------------------------------------------- #
+
+def user_in_voice_channel(ctx):
+    """Returns true if the user is in a voice channel, false otherwise."""
+    if ctx.author.voice:
+        return True
+    else:
+        return False
+
+def bot_in_voice_channel(ctx):
+    """Returns true if the bot is in a voice channel, false otherwise."""
+    if ctx.voice_client:
+        return True
+    else:
+        return False
+
+def bot_in_user_voice_channel(ctx):
+    """Returns true if the bot and user are in the same voice channel, false otherwise."""
+    if not user_in_voice_channel(ctx) or not bot_in_voice_channel(ctx):
+        return False
+
+    USER_VOICE_CHANNEL = ctx.author.voice.channel
+    BOT_VOICE_CHANNEL = ctx.guild.voice_client
+
+    return (BOT_VOICE_CHANNEL == USER_VOICE_CHANNEL)
+
+# ---------------------------------------------- begin commands ---------------------------------------------- #
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send("test message")
+async def join(ctx):
+    """Gets the bot to join the caller's voice channel."""
+
+    if not user_in_voice_channel(ctx):
+        await ctx.send("Error: Need to be in a voice channel.")
+        return
+    
+    voice_channel = ctx.author.voice.channel
+
+    # if already in a voice channel, disconnect first
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+
+    await voice_channel.connect()
+
+@bot.command()
+async def leave(ctx):
+    """Gets the bot to leave its current voice channel."""
+
+    if not ctx.voice_client:
+        await ctx.send("Error: Bot is not in a voice channel.")
+        return
+    
+    if not bot_in_user_voice_channel(ctx):
+        await ctx.send("Error: Cannot disconnect bot without being in the same voice channel")
+        return
+    
+    await ctx.voice_client.disconnect()
 
 bot.run(BOT_KEY)
